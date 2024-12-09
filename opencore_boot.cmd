0a9c857119c11a6f54e4591ae90f713cc82c9224
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:00:28 +0100

    Update opencore_boot.cmd

--- opencore_boot.cmd
+++ opencore_boot.cmd
@@ -1,9 +1,7 @@
 @echo off
 :boothome
 echo ^>Loading initial RamDisk device
-choice /c OR /m "Press O to boot from OpenCore kernel or press R to boot into rescue mode."
-if %errorlevel% == 1 goto boot
-if %errorlevel% == 2 goto rescue
+goto boot
 :boot
 echo Booting from Poggersire DOS OpenCore kernel
 echo.
@@ -13,16 +11,3 @@ echo   kernel instead of using a custom made kernel.
 echo.
 pause
 exit
-:rescue
-echo.
-echo Starting OpenCore rescue mode
-echo.
-echo Troubleshooting...
-timeout 5 /nobreak >null
-echo Fixing problems...
-timeout 3 /nobreak >null
-echo Rebooting PC...
-timeout 1 /nobreak >null
-del null
-cls
-goto boothome
