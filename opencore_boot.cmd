2d9d8c969984ac91f12d19713187ed21de975fb7
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:42:22 +0100

    Update opencore_boot.cmd

--- opencore_boot.cmd
+++ opencore_boot.cmd
@@ -4,6 +4,12 @@ cls
 echo ^>Loading initial RamDisk device
 goto boot
 :boot
-echo Booting from Poggersire DOS OpenCore kernel
+cls
+color 80
+echo Poggersire DOS OpenCore Kernel 1.0
+echo Kernel started from folder %cd%
+echo EFI system partition located at /EFI.
+echo Date and time is %date% %time%
+echo.
 echo.
 startupscripts.bat
