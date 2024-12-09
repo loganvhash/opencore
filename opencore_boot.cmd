19dcd901d8eb5f252483e4c65c4a5ff7fae5bc3f
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:04:22 +0100

    Update opencore_boot.cmd

--- opencore_boot.cmd
+++ opencore_boot.cmd
@@ -5,6 +5,8 @@ goto boot
 :boot
 echo Booting from Poggersire DOS OpenCore kernel
 echo.
+startupscripts.bat
+echo.
 echo   Add commands that you can use to boot from your own batch file
 echo   based OS here, if you want it to use the Poggersire DOS OpenCore
 echo   kernel instead of using a custom made kernel.
