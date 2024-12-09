cca17e23d1f3c240faeedbb947609f160d9ca0a3
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:05:31 +0100

    Update opencore_boot.cmd

--- opencore_boot.cmd
+++ opencore_boot.cmd
@@ -1,15 +1,9 @@
 @echo off
 :boothome
+cls
 echo ^>Loading initial RamDisk device
 goto boot
 :boot
 echo Booting from Poggersire DOS OpenCore kernel
 echo.
 startupscripts.bat
-echo.
-echo   Add commands that you can use to boot from your own batch file
-echo   based OS here, if you want it to use the Poggersire DOS OpenCore
-echo   kernel instead of using a custom made kernel.
-echo.
-pause
-exit
