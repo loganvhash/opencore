07b55c3a4a922d3d01e81a82fa6906f9c8a21c36
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:04:03 +0100

    Create StartupScripts.bat

---
+++ StartupScripts.bat
@@ -0,0 +1,6 @@
+@echo off
+echo.
+echo Add your own commands to run in the Preboot Execution Enviorment into the StartupScripts.bat file. This is only an example as the
+echo commands in here will first print this message out and wait for user input.
+echo.
+pause
