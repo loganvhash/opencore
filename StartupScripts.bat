9ec157bfcdc9efd3fc355dce431e0efd290f1cdd
Poggersire Industries <lzvd9001@gmail.com>
2024-12-09 16:11:29 +0100

    Update StartupScripts.bat

--- StartupScripts.bat
+++ StartupScripts.bat
@@ -3,4 +3,9 @@ echo.
 echo Add your own commands to run in the Preboot Execution Enviorment into the StartupScripts.bat file. This is only an example as the
 echo commands in here will first print this message out and wait for user input.
 echo.
-pause
+echo You can add commands used for booting a operating system or others.
+echo.
+echo Press any key to shutdown.
+pause >null
+del null
+exit
